30e3172f3d0caf4f2e45676e11490130f8968f6d
fatcloud <cloud@seabunny.tech>
2022-07-06 15:31:55 +0800

    自動偵測 numpy 影像的格式再顯示出來

--- kivyguidescreen/widgets/numpyimage.py
+++ kivyguidescreen/widgets/numpyimage.py
@@ -10,13 +10,23 @@ class NumpyImage(Widget):
     def __init__(self, npimage, **kwargs):
         super().__init__(**kwargs)
 
-        w, h = [npimage.shape[i] for i in [0, 1]]
-        texture = Texture.create(size=(h, w), colorfmt='rgb', bufferfmt='ubyte')
-        
-        if len(npimage.shape) < 3:
+        # 記下各種重要資訊
+        w, h = self.shape = [npimage.shape[i] for i in [0, 1]]
+
+        # 看 shape 決定顏色格式
+        colorfmt = None
+        if len(npimage.shape) == 2:
+            colorfmt = 'rgb'
             npimage = cv2.cvtColor(npimage, cv2.COLOR_GRAY2RGB)
 
-        texture.blit_buffer(npimage.flatten(), colorfmt='rgb', bufferfmt='ubyte')
+        elif len(npimage.shape) == 3:
+            num_channels = npimage.shape[2]
+            colorfmt = {3:'rgb', 4:'rgba'}[num_channels]
 
+        # 產生 Texture 並將 numpy array 灌進去
+        texture = Texture.create(size=(h, w), colorfmt=colorfmt, bufferfmt='ubyte')
+        texture.blit_buffer(npimage.flatten(), colorfmt=colorfmt, bufferfmt='ubyte')
+
+        # 把影像交給 image
         w_img = Image(size=(h, w), texture=texture)
         self.add_widget(w_img)
