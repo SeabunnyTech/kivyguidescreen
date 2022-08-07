11e52b52434b6a994ee6c4e6c7e43902f11bed0d
fatcloud <cloud@seabunny.tech>
2022-08-08 02:25:21 +0800

    重製 GridEditor 使它只要輸入 shape = (2,2) 就可以代替 QuadEditor

--- kivyguidescreen/widgets/grideditor.py
+++ kivyguidescreen/widgets/grideditor.py
@@ -7,69 +7,83 @@ from kivy.clock import Clock
 
 from time import time
 
-class GridNode:
 
-    def __init__(self, xy):
-        self.xy = xy
+class Node:
 
-    def set_index(self, row, column):
-        self._row = row
-        self._column = column
+    def __init__(self, pos, xy):
+        self._pos = pos
+        self.xy = tuple(xy)
 
-
-
-class MappingGrid:
+    @property
+    def pos(self):
+        return self._pos
 
 
-    def __init__(self, griddata):
+class Grid:
+
+    def __init__(self, shape=None, coords=None, **kw):
+        if shape is not None:
+            if coords is not None:
+                if kw != {}:
+                    raise RuntimeError("No parameters are expected when \"coords\" are specified: " + str(**kw) + "")
+                self.load(shape, coords)
+            else:
+                self.init(shape, **kw)
+
+
+    def load(self, shape, coords):
+        num_rows, num_cols = self._shape = shape
+        assert num_rows * num_cols == len(coords)
+
         # 載入節點
-        self._griddata = griddata
-        self._gridsize = griddata['size']
-        self._nodes = nodes = [GridNode(xy=xy) for xy in griddata['nodes']]
+        self._nodes = []
+        for index, coord in enumerate(coords):
+            row, col = index // num_cols, index % num_cols
+            node = Node(pos=[row, col], xy=coord)
+            self._nodes.append(node)
 
 
-    def size(self):
-        return self._gridsize
+    def init(self, shape, pos=[0, 0], size=[100, 100]):
+        num_rows, num_cols = shape
+        if num_rows < 2 or num_cols < 2:
+            raise ValueError("At least 2 rows and 2 columns are expected")
+
+        # 計算每個頂點的水平和垂直間隔
+        w, h = size
+        row_step = h / (num_rows - 1)
+        col_step = w / (num_cols - 1)
+
+        # 計算並加入每個節點的座標
+        coords = []
+        for row in range(num_rows):
+            for col in range(num_cols):
+                x0, y0 = pos
+                coord = (x0 + col_step * col, y0 + row_step * row)
+                coords.append(coord)
+
+        self.load(shape, coords)
+
+
+    @property
+    def shape(self):
+        return self._shape
 
 
     def node(self, row, column):
         # 節點儲存時已經過排序
-        w, h = self.size()
-        return self._nodes[column + w * row]
+        num_rows, num_cols = self.shape
+        return self._nodes[column + num_cols * row]
 
 
     @property
     def griddata(self):
-        num_row, num_col = self.size()
-        
-        nodes = []
-        for row in range(num_row):
-            for col in range(num_col):
-                n = self.node(row, col)
-                nodes.append(n.xy)
-
-        return dict(size=self.size(), nodes=nodes)
-
-
-    def size(self):
-        return self._gridsize
-
-
-    def subdivide(self, w, h):
-        # add more nodes into the grid
-        # meanwhile keep the old nodes' properties
-        pass
-
-
-    @property
-    def corners(self):
-        return [node.xy for node in self._nodes]
+        return dict(shape=self.shape, coords=[node.xy for node in self._nodes])
 
 
     def closest_node(self, x, y):
         min_distance_square = 100000000
         closest_node = None
-        row_count, column_count = self._gridsize
+        row_count, column_count = self.shape
         for c in range(column_count):
             for r in range(row_count):
                 nx, ny = self.node(r, c).xy
@@ -85,18 +99,23 @@ class MappingGrid:
 
 class GridEditor(Widget):
 
+    init_corner_offset = NumericProperty(100)
 
     # 線條的寬度與顏色
     line_width = NumericProperty(1)
     line_color = ColorProperty('yellow')
     blink_color = ColorProperty('white')
 
-    def __init__(self, griddata=None, **kw):
+    def __init__(self, shape=None, nodes=None, **kw):
         super().__init__(**kw)
 
-        self._mapping_grid = None
-        if griddata is not None:
-            self.load_grid(griddata=griddata)
+        # 選擇載入 / 新增 或者只產生空的 Grid
+        self._grid = None
+        if shape is not None:
+            if nodes is not None:
+                self._load_grid(shape=shape, nodes=nodes)
+            else:
+                self._init_grid(shape)
 
         self._is_dragging = False
         self._selected_node = None
@@ -117,24 +136,31 @@ class GridEditor(Widget):
                 self._render_routine = Clock.schedule_interval(self.update_canvas, 1/60)
 
 
-    def load_grid(self, griddata):
-        self._mapping_grid = MappingGrid(griddata=griddata)
+    def init_grid(self, shape=[2,2]):
+        of = self.init_corner_offset
+        w, h = self.size
+        self._grid = Grid(shape=shape, pos=[of, of], size=[w-2*of, h-2*of])
+
+
+    def load_grid(self, shape, coords):
+        self._grid = Grid(shape=shape, coords=coords)
 
 
     @property
     def griddata(self):
-        if self._mapping_grid is None:
+        if self._grid is None:
             raise RuntimeError("Trying to access griddata before initializing one")
 
-        return self._mapping_grid.griddata
+        return self._grid.griddata
 
 
     def selected_node_description(self):
-        return str([round(v) for v in self._selected_node.xy])
+        return str([round(v, 2) for v in self._selected_node.xy])
 
 
     def update_canvas(self, *dt):
-        if self._mapping_grid is None:
+        grid = self._grid
+        if grid is None:
             return
 
         cursor_position = list(self.to_widget(*Window.mouse_pos))
@@ -145,18 +171,25 @@ class GridEditor(Widget):
         canvas.clear()
         canvas.add(Color(*self.line_color))
 
-        # 畫出四條連接線
-        p00, pw0, p0h, pwh = [list(p) for p in self._mapping_grid.corners]
-        edges = [p00+pw0, pw0+pwh, pwh+p0h, p00+p0h]
-        for edge in edges:
-            canvas.add(Line(points=edge, width=self.line_width))
+        # 所有的橫格線
+        row_num, col_num = grid.shape
+        for row in range(row_num):
+            for col in range(col_num - 1):
+                points = grid.node(row, col).xy + grid.node(row, col+1).xy
+                canvas.add(Line(points=points, width=self.line_width))
+
+        # 直格線
+        for row in range(row_num - 1):
+            for col in range(col_num):
+                points = grid.node(row, col).xy + grid.node(row+1, col).xy
+                canvas.add(Line(points=points, width=self.line_width))
 
         if self.disabled:
             return
 
         # 如果不是正在拖曳中的話就可以即時重選最接近的節點當作 selected_node
         if not self._is_dragging or self._selected_node is None:
-            self._selected_node = self._mapping_grid.closest_node(*cursor_position)
+            self._selected_node = self._grid.closest_node(*cursor_position)
 
         # 畫出選擇線
         selection_line_color = self.blink_color if time() > self._blink_time else self.line_color
