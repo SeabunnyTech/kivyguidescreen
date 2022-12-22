7793b1c3735f36ef8ea58e0e775fba8249a60a5b
DESKTOP-UAROS0B\seabunny <cloud@seabunny.tech>
2022-12-22 11:24:11 +0800

    引入 GuideScreenVariable 重寫 VerifyAruco 頁面

--- kivyguidescreen/screensplus/verifyaruco.py
+++ kivyguidescreen/screensplus/verifyaruco.py
@@ -2,14 +2,127 @@ from kivy.properties import NumericProperty, ListProperty, StringProperty, Color
 from kivy.graphics import Line, Color
 from kivy.clock import Clock, mainthread
 
-from kivyguidescreen import GuideScreen
+from kivyguidescreen import GuideScreen, GuideScreenVariable
 from kivyguidescreen.widgets.grideditor import Grid
 
 import cv2
 import numpy as np
 from kivyguidescreen.utils.armath import PerspectiveTransform
 
+
 class VerifyArucoScreen(GuideScreen):
+
+    tracker2d_source = StringProperty()
+    tracker_height = NumericProperty(0)
+
+    lens_center_xyz = GuideScreenVariable()
+    table_points_pixel = GuideScreenVariable()
+    table_points_mm = GuideScreenVariable()
+
+    paused = BooleanProperty(False)
+
+    def on_enter(self):
+        # 建立桌面座標到像素的轉換
+        table_points_pixel = self.table_points_pixel.read().coords
+        table_points_mm = self.table_points_mm.read().coords
+        self._xy_to_uv_transform = PerspectiveTransform(src_points=table_points_mm, dst_points=table_points_pixel)
+
+        # 取得鏡心
+        self._lens_center_xyz = self.lens_center_xyz.read()
+
+        # 固定索取 aruco 數據
+        self._routine = Clock.schedule_interval(self._retrieve_aruco, 1/30)
+
+
+    def xyz_to_uv(self, xyz):
+        cx, cy, cz = self._lens_center_xyz
+        hx, hy, hz = xyz
+        head_xy = hx, hy
+        
+        dxy_per_z = np.array([hx-cx, hy-cy]) / (cz-hz)
+        tail_xy = np.array(head_xy) + dxy_per_z * hz
+        uv = self._xy_to_uv_transform.apply(tail_xy)
+        
+        return uv
+
+
+    def on_leave(self):
+        if self._routine:
+            self._routine.cancel()
+            self._routine = None
+
+
+    def _retrieve_aruco(self, dt):
+        if self.paused:
+            return
+
+        self.socketio_client.emit(event=self.tracker2d_source.lower(), data='', namespace=None, callback=self._on_receive_aruco)
+
+    def on_paused(self, caller, value):
+        if not self.paused:
+            self.socketio_client.emit(
+                event="set_config",
+                data=dict(
+                    path=self.tracker2d_source.lower(),
+                    config={'compensate_to' : {'0':self._last_pose}}
+                    ),
+                namespace=None)
+
+
+    def on_press_space(self):
+        self.paused = not self.paused
+
+
+    @mainthread
+    def _on_receive_aruco(self, message):
+        self.guide = '檢查投影輪廓是否正確框住校正器'
+        corners = message['corners']
+        canvas = self.canvas.after
+        canvas.clear()
+        
+        def draw_line(xy_points, z=self.tracker_height, close=False):
+            uvs = [self.xyz_to_uv([*xy, z]).tolist() for xy in xy_points]
+            if close:
+                uvs = uvs + [uvs[0]]
+            canvas.add(Line(points=sum(uvs, [])))
+
+        # 畫出參考點
+        canvas.add(Color(1, 1, 0))
+        p1, p2, p4, p3 = Grid(**self.table_points_mm.read()).coords()
+        draw_line(xy_points = [p1, p2, p3, p4], z=0, close=True)
+
+        # 畫出校正器外框
+        canvas.add(Color(1, 1, 1))
+        for contour in corners.values():
+            draw_line(xy_points=contour, close=True)
+
+        # 畫出位置補償後的中心點
+        canvas.add(Color(1, 0, 0))
+        if 'pose_ue4' in message and '0' in message['pose_ue4']:
+            pose_ue4 = message['pose_ue4']['0']
+            yc, xc, _ =  pose_ue4['location']
+            _, _, yaw = pose_ue4['rotation']
+
+            rad_yaw = -np.deg2rad(yaw)
+            cos, sin = np.cos(rad_yaw), np.sin(rad_yaw)
+            cen = np.array([xc, yc])
+            hvec = np.array([cos, sin])
+            vvec = np.array([-sin, cos])
+            r = 10
+            draw_line(xy_points=[cen-20*hvec, cen+20*hvec])
+            draw_line(xy_points=[cen-15*hvec+10*vvec, cen-15*hvec-10*vvec])
+            draw_line(xy_points=[cen+15*hvec+10*vvec, cen+15*hvec-10*vvec])
+
+            # 把當前位置記下來隨時準備重設 compensation
+            self._last_pose = dict(pos=[yc, xc], yaw=yaw)
+
+
+    def on_press_enter(self):
+        self.goto_next_screen()
+
+
+
+class VerifyArucoScreenOld(GuideScreen):
 
     aruco_line_color = ColorProperty([1, 1, 0])
 
